8bf6b7ab75ccc1d53fe9425992c9de7a0a0de5f3
Ermiyas9 <115026605+Ermiyas9@users.noreply.github.com>
2026-04-13 19:10:04 -0400

    so I changed the project styling to Mantine: installed Mantine packages and wrapped the app/layout with MantineProvider. Updated components to use Mantine UI instead of custom CSS

--- BudgetingWebApplication/budgetingwebapp.client/src/pages/LandingPage.tsx
+++ BudgetingWebApplication/budgetingwebapp.client/src/pages/LandingPage.tsx
@@ -1,20 +1,41 @@
+import { Button, Center, Stack, Text, Title } from "@mantine/core";
+import { useNavigate } from "react-router-dom";
+
+
 export default function LandingPage() {
 
+const navigate = useNavigate();
 
+  const handleStart = () => {
+    const user = JSON.parse(localStorage.getItem("user") || "null");
+    const isLoggedIn = !!user;
+
+    if (isLoggedIn) {
+      navigate("/dashboard");
+    } else {
+      navigate("/login");
+    }
+  };
   return (
-    <div style={{ 
-      height: "80vh",
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "center",
-      alignItems: "center",
-      gap: "20px"
-    }}>
-      
-      <h1>Butgeting App</h1>
-      <p>Landing page</p>
+    <Center h="85vh">
+      <Stack align="center" gap="xl">
+        <Stack align="center" gap="xs">
+          <Title size={40} fw={900} style={{ whiteSpace: "nowrap" }}>
+            Budgeting App
+          </Title>
 
-  
-    </div>
-  )
+          <Text fw={800} c="dimmed" tt="uppercase">
+            Track your expenses. Manage your money.
+          </Text>
+        </Stack>
+
+        <Stack align="center" gap="md" w={350}>
+          {/* Start Button */}
+          <Button onClick={handleStart} size="lg" variant="filled" fullWidth>
+            Start Budgeting
+          </Button>
+        </Stack>
+      </Stack>
+    </Center>
+  );
 }
