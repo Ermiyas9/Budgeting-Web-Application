9d561c9691b8afa88b3aa674abd19ef641ead828
Ermiyas9 <115026605+Ermiyas9@users.noreply.github.com>
2026-04-13 18:07:32 -0400

    Added layout page so each page will have similar display

--- BudgetingWebApplication/budgetingwebapp.client/src/App.tsx
+++ BudgetingWebApplication/budgetingwebapp.client/src/App.tsx
@@ -2,15 +2,19 @@ import { Routes, Route } from "react-router-dom"
 import HomePage from "./pages/HomePage"
 import LandingPage from "./pages/LandingPage"
 import LoginPage from "./pages/LoginPage"
+import Layout from "./Layout"
 
 
 
 function App() {
     return (
         <Routes>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/home" element={<HomePage />} />
-            <Route path="/login" element={<LoginPage />} />
+            <Route path="/" element={<Layout />}>
+                <Route index element={<LandingPage />} />
+                <Route path="home" element={<HomePage />} />
+                <Route path="login" element={<LoginPage />} />
+            </Route>    
+       
         </Routes>
 
     )
---
+++ BudgetingWebApplication/budgetingwebapp.client/src/Layout.tsx
@@ -0,0 +1,31 @@
+import { Outlet, Link } from "react-router-dom"
+
+export default function Layout() {
+  return (
+    <div>
+      {/* header we willl cchange this page later on */}
+      <header
+        style={{
+          padding: "10px 20px",
+          borderBottom: "1px solid #ddd",
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center"
+        }}
+      >
+        <h2 style={{ margin: 0 }}>Budgeting App</h2>
+
+        <nav style={{ display: "flex", gap: "15px" }}>
+          <Link to="/">Landing</Link>
+          <Link to="/home">Home</Link>
+          <Link to="/login">Login</Link>
+        </nav>
+      </header>
+
+      {/* Page content */}
+      <main style={{ padding: "20px" }}>
+        <Outlet />
+      </main>
+    </div>
+  )
+}
