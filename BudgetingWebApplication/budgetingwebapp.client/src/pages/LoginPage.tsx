9ebf2288dd1784689855a2ec55bae2c10df61b96
Ermiyas9 <115026605+Ermiyas9@users.noreply.github.com>
2026-04-13 23:05:15 -0400

    Added login logic, the password will be replaced by google auth later

--- BudgetingWebApplication/budgetingwebapp.client/src/pages/LoginPage.tsx
+++ BudgetingWebApplication/budgetingwebapp.client/src/pages/LoginPage.tsx
@@ -1,20 +1,47 @@
-export default function LoginPage() {
+import { useState } from "react";
+import { Button, Center, Stack, TextInput, PasswordInput, Title, Text } from "@mantine/core";
 
+
+export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+    // here i will add the login logic later on, for now it will just navigate to homepage
+    const handleLogin = () => {
+        // i might change this later when i impeleting google login. 
+        if (email === "" || password === "") {
+            alert("Please enter email and password.");
+            return;
+        }   
+    };
 
   return (
-    <div style={{ 
-      height: "80vh",
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "center",
-      alignItems: "center",
-      gap: "20px"
-    }}>
-      
-      <h1>Butgeting App</h1>
-      <p>Login Page</p>
+     <Center h="85vh">
+      <Stack w={320} align="center" gap="lg">
+        <Title order={2}>Budgeting App</Title>
+        <Text c="dimmed">Login to continue</Text>
 
-  
-    </div>
-  )
+        <TextInput
+          label="Email"
+          placeholder="you@example.com"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+
+        // this i will change it with google login latoer on. 
+        <PasswordInput
+          label="Password"
+          placeholder="Your password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+
+        <Button fullWidth onClick={handleLogin}>
+          Login
+        </Button>
+      </Stack>
+    </Center>
+  );
 }
